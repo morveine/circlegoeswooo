ba42d5b17fadb78fb57a03c4d63f6234e1d445f0
morveine <morveine@gmail.com>
2023-11-04 15:15:14 +0100

    start js, fix typo

---
+++ script.js
@@ -0,0 +1,17 @@
+const circle = document.getElementById('circle-form');
+const sizeInput = document.getElementById('slider-size');
+const colorInput = document.getElementById('color-picker');
+
+// todo: change size, change color, make circle draggable
+
+sizeInput.addEventListener('input', changeSize);
+colorInput.addEventListener('input', changeColor);
+circle.addEventListener('click', dragElement);
+
+
+function changeColor() {
+    
+};
+
+function changeSize() { };
+function dragElement(){};
