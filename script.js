729f276156abffc4a16431dd78cac2256983a991
morveine <morveine@gmail.com>
2023-11-06 21:51:04 +0100

    add controls and drag

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 const circle = document.getElementById('circle-form');
+const circleDiv = document.getElementById('circle-div');
 const sizeInput = document.getElementById('slider-size');
 const colorInput = document.getElementById('color-picker');
-
-// todo: change size, change color, make circle draggable
+const root = document.documentElement;
+let mousePosition;
+let offset = [0, 0];
+let isDown = false;
 
 sizeInput.addEventListener('input', changeSize);
 colorInput.addEventListener('input', changeColor);
-circle.addEventListener('click', dragElement);
+// circle.addEventListener('click', dragElement);
+circleDiv.addEventListener('mousedown', function (e) {
+    isDown = true;
+    offset = [
+        circleDiv.offsetLeft - e.clientX,
+        circleDiv.offsetTop - e.clientY
+    ];
+}, true);
+
+document.addEventListener('mouseup', function () {
+    isDown = false;
+}, true);
+
+document.addEventListener('mousemove', function (event) {
+    if (isDown) {
+        event.preventDefault();
+        mousePosition = {
+
+            x: event.clientX,
+            y: event.clientY
+
+        };
+        circleDiv.style.left = (mousePosition.x + offset[0]) + 'px';
+        circleDiv.style.top = (mousePosition.y + offset[1]) + 'px';
+    }
+}, true);
 
 
 function changeColor() {
-    
+    circle.style.backgroundColor = colorInput.value;
 };
 
-function changeSize() { };
-function dragElement(){};
+function changeSize() {
+    root.style.setProperty('--circle-dimension', sizeInput.value + 'px')
+};
+
+function dragElement() { };
